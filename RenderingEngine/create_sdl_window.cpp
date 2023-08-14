575a31ade8cee9d5f061535f7463c1e4947bab66
Timothy Young <timothyoung97@gmail.com>
2023-08-14 17:14:20 +0800

    Refactor event loop

--- RenderingEngine/create_sdl_window.cpp
+++ RenderingEngine/create_sdl_window.cpp
@@ -1,9 +1,8 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2023
-and may not be redistributed without written permission.*/
-
 //Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
+
+#include <cassert>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
@@ -18,32 +17,32 @@ int main(int argc, char* args[])
 	SDL_Surface* screenSurface = NULL;
 
 	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	assert(SDL_Init(SDL_INIT_VIDEO) > -1);
+
+	//Create window
+	window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	assert(window != NULL);
+
+	//Get window surface
+	screenSurface = SDL_GetWindowSurface(window);
+
+	SDL_Event e; 
+	bool quit = false; 
+
+	while (quit == false) 
 	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
+		//Fill the surface white
+		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+		//Update the surface
+		SDL_UpdateWindowSurface(window);
 
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Hack to get window to stay up
-			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
-		}
+		while (SDL_PollEvent(&e)) 
+		{ 
+			if (e.type == SDL_QUIT) 
+				quit = true; 
+		} 
 	}
 
 	//Destroy window
