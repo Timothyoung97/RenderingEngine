d9453eedc10e65e70bfd4ab084be9aece2982edf
Timothy Young <timothyoung97@gmail.com>
2023-08-18 10:41:43 +0800

    Update triangle shader

--- RenderingEngine/shaders/triangle.hlsl
+++ RenderingEngine/shaders/triangle.hlsl
@@ -4,9 +4,9 @@ void vs_main (
 ) 
 {
     const float4 pos[3] = {
-        float4(0, 0.75, 0, 1),
-        float4(0.5, 0, 0, 1),
-        float4(0, 0, 0.5, 1)
+        float4(0, 0.5, 0, 1),
+        float4(0.5, -0.5, 0, 1),
+        float4(-0.5, -0.5, 0, 1)
     };
 
     outPosition = pos[vertexID];
