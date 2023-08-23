4d9a4c0936d1c0662b6f86a597a3c6d276b996e5
Timothy Young <timothyoung97@gmail.com>
2023-08-23 17:32:17 +0800

    Refactor dxdebug to take in dxfunction and its argv together

--- RenderingEngine/dxdebug.h
+++ RenderingEngine/dxdebug.h
@@ -2,10 +2,10 @@
 #include <string>
 #include <cassert>
 
-#define CHECK_DX_ERROR(dx11Func, ...) \
+#define CHECK_DX_ERROR(dx11Func) \
 { \
 	HRESULT hresult; \
-	if (!SUCCEEDED(hresult = dx11Func(__VA_ARGS__))) { \
+	if (!SUCCEEDED(hresult = dx11Func)) { \
 		std::printf("Assertion failed: %d at %s:%d\n", hresult, __FILE__, __LINE__);	\
 		assert(false); \
 	} \
