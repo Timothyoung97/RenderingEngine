a092dfbda7f17cc99a8bb3e90787bda530ad1818
Timothy Young <timothyoung97@gmail.com>
2023-08-24 18:01:16 +0800

    Update raw pointer in factory to smart pointer

--- RenderingEngine/factory.cpp
+++ RenderingEngine/factory.cpp
@@ -1,5 +1,7 @@
 #include "factory.h"
 
+using Microsoft::WRL::ComPtr;
+
 namespace tre {
 
 Factory::Factory() {
@@ -9,32 +11,23 @@ Factory::Factory() {
 void Factory::InitFactory() {
 
 	//Create dxgiFactory
-
 	CHECK_DX_ERROR(CreateDXGIFactory2(
-		DXGI_CREATE_FACTORY_DEBUG, __uuidof(IDXGIFactory2), reinterpret_cast<void**>(&dxgiFactory2)
+		DXGI_CREATE_FACTORY_DEBUG, __uuidof(IDXGIFactory2), &dxgiFactory2
 	));
 
 	//Create DXGI debug layer
-	IDXGIDebug1* dxgiDebug = nullptr;
+	ComPtr<IDXGIDebug1> dxgiDebug;
 	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug))))
 	{
 		dxgiDebug->EnableLeakTrackingForThread();
-		IDXGIInfoQueue* dxgiInfoQueue = nullptr;
+		ComPtr<IDXGIInfoQueue> dxgiInfoQueue = nullptr;
 		if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiInfoQueue))))
 		{
-
 			dxgiInfoQueue->SetBreakOnSeverity(dxgi_debug_all, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_CORRUPTION, true);
 			dxgiInfoQueue->SetBreakOnSeverity(dxgi_debug_all, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_ERROR, true);
 			dxgiInfoQueue->SetBreakOnSeverity(dxgi_debug_all, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_WARNING, true);
-
-			dxgiInfoQueue->Release();
-			dxgiDebug->Release();
 		}
 	}
-}
-
-Factory::~Factory() {
-	dxgiFactory2->Release();
 }
 
 }
--- RenderingEngine/factory.h
+++ RenderingEngine/factory.h
@@ -4,6 +4,7 @@
 #include <dxgi1_2.h>
 #include <dxgi1_3.h>
 #include <dxgidebug.h>
+#include <wrl/client.h>
 
 #include "dxdebug.h"
 
@@ -13,13 +14,12 @@ class Factory {
 	const GUID dxgi_debug_all = { 0xe48ae283, 0xda80, 0x490b, { 0x87, 0xe6, 0x43, 0xe9, 0xa9, 0xcf, 0xda, 0x8 } };
 
 public:
-	IDXGIFactory2* dxgiFactory2 = nullptr;
+	Microsoft::WRL::ComPtr<IDXGIFactory2> dxgiFactory2;
 
 	Factory();
 
 	void InitFactory();
 
-	~Factory();
 	Factory(const Factory&) = delete;
 	Factory& operator=(const Factory&) = delete;
 };
