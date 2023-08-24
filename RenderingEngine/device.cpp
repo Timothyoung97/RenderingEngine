8d283801c5c8272985556023693bbd2c3062f05d
Timothy Young <timothyoung97@gmail.com>
2023-08-24 18:05:14 +0800

    Remove void** casting

--- RenderingEngine/device.cpp
+++ RenderingEngine/device.cpp
@@ -22,13 +22,13 @@ void Device::InitDXDevice() {
 	ComPtr<ID3D11Debug> d3dDebug;
 
 	CHECK_DX_ERROR( device->QueryInterface(
-		__uuidof(ID3D11Debug), (void**)&d3dDebug
+		__uuidof(ID3D11Debug), &d3dDebug
 	));
 	
 	ComPtr<ID3D11InfoQueue> d3dInfoQueue;
 
 	CHECK_DX_ERROR( d3dDebug->QueryInterface(
-		__uuidof(ID3D11InfoQueue), (void**)&d3dInfoQueue
+		__uuidof(ID3D11InfoQueue), &d3dInfoQueue
 	));
 
 	d3dInfoQueue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_CORRUPTION, true);
