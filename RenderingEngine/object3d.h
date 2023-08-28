5410774c3a0ecab20aaa6989e2cb2b0acd40dec3
Timothy Young <timothyoung97@gmail.com>
2023-08-28 17:20:14 +0800

    Overload cube and sphere's constructor
    To add randomized parameters in constructor

--- RenderingEngine/object3d.h
+++ RenderingEngine/object3d.h
@@ -24,12 +24,17 @@ class Cube3d : public Object3d {
 public:
 
 	Cube3d(float unitLength);
+	Cube3d(XMFLOAT3 origin, float unitLength);
+	void create(XMFLOAT3 origin, float unitLength);
 };
 
 class Sphere3d : public Object3d {
 public:
 
-	Sphere3d(float r, int sectorCount, int stackCount);
+	Sphere3d(float r, int sectorC, int stackC);
+	Sphere3d(XMFLOAT3 origin, float r, int sectorC, int stackC);
+	
+	void create(XMFLOAT3 origin, float r, int sectorC, int stackC);
 	XMFLOAT3 findCoordinate(XMFLOAT3 unitVector, float radius);
 };
 }
