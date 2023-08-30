6ccd225916d4d21391b1dc0c1068b7e86ab99389
Timothy Young <timothyoung97@gmail.com>
2023-08-30 14:56:26 +0800

    Refactor main with mesh and texture class

--- RenderingEngine/main.cpp
+++ RenderingEngine/main.cpp
@@ -8,9 +8,6 @@
 #include <wrl/client.h>
 #include "spdlog/spdlog.h"
 
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
-
 //Custom Header
 #include "timer.h"
 #include "window.h"
@@ -23,6 +20,7 @@
 #include "utility.h"
 #include "camera.h"
 #include "constbuffer.h"
+#include "texture.h"
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 800;
@@ -95,83 +93,22 @@ int main()
 
 	// 3D object
 	tre::CubeMesh cube = tre::CubeMesh(deviceAndContext.device.Get());
+	tre::SphereMesh sphere = tre::SphereMesh(deviceAndContext.device.Get(), 10, 10);
 
 	//Set index buffer
-	deviceAndContext.context->IASetIndexBuffer(cube.pIndexBuffer, DXGI_FORMAT_R16_UINT, 0);
+	deviceAndContext.context->IASetIndexBuffer(cube.pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0);
 
 	//Set vertex buffer
 	UINT vertexStride = sizeof(Vertex);
 	UINT offset = 0;
-	deviceAndContext.context->IASetVertexBuffers(0, 1, &cube.pVertexBuffer, &vertexStride, &offset);
+	deviceAndContext.context->IASetVertexBuffers(0, 1, cube.pVertexBuffer.GetAddressOf(), &vertexStride, &offset);
 
-	// load image using stb
-	string texturePath = util.basePathStr + "textures\\UV_image.jpg";
-
-	int imgWidth, imgHeight, imgChannels, desiredChannels = 4;
-	unsigned char* img = stbi_load(texturePath.c_str(), &imgWidth, &imgHeight, &imgChannels, desiredChannels);
-	if (img == NULL) {
-		spdlog::error("Error loading image");
-	}
-	spdlog::info("Img width: {}, Img height: {}, Img channels: {}\n", imgWidth, imgHeight, imgChannels);
-
-	// Create texture
-	D3D11_TEXTURE2D_DESC texture2dDesc;
-	texture2dDesc.Width = imgWidth;
-	texture2dDesc.Height = imgHeight;
-	texture2dDesc.MipLevels = 1;
-	texture2dDesc.ArraySize = 1;
-	texture2dDesc.SampleDesc.Count = 1;
-	texture2dDesc.SampleDesc.Quality = 0;
-	texture2dDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	texture2dDesc.Usage = D3D11_USAGE_DYNAMIC;
-	texture2dDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
-	texture2dDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
-	texture2dDesc.MiscFlags = 0;
-
-	D3D11_SUBRESOURCE_DATA textureData = {};
-	textureData.pSysMem = img;
-	textureData.SysMemPitch = imgWidth * 4;
-	textureData.SysMemSlicePitch = textureData.SysMemPitch * imgHeight;
-
-	ComPtr<ID3D11Texture2D> pTexture;
-
-	CHECK_DX_ERROR(deviceAndContext.device->CreateTexture2D(
-		&texture2dDesc, &textureData, pTexture.GetAddressOf()
-	));
-
-	STBI_FREE(img);
-
-	// Create Shader Resource view
-	D3D11_SHADER_RESOURCE_VIEW_DESC shaderResViewDesc;
-	shaderResViewDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	shaderResViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-	shaderResViewDesc.Texture2D = D3D11_TEX2D_SRV(0, -1);
-
-	ComPtr<ID3D11ShaderResourceView> pShaderResView;
-
-	CHECK_DX_ERROR(deviceAndContext.device->CreateShaderResourceView(
-		pTexture.Get(), &shaderResViewDesc, pShaderResView.GetAddressOf()
-	));
-
-	// Create sampler state
-	D3D11_SAMPLER_DESC samplerDesc;
-	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
-	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
-	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
-	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
-	samplerDesc.MipLODBias = 0;
-	samplerDesc.MaxAnisotropy = 1;
-	samplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
-
-	ComPtr<ID3D11SamplerState> pSamplerState;
-
-	CHECK_DX_ERROR(deviceAndContext.device->CreateSamplerState(
-		&samplerDesc, pSamplerState.GetAddressOf()
-	));
+	//Create texture
+	tre::Texture texture(deviceAndContext.device.Get(), util.basePathStr + "textures\\UV_image.jpg");
 
 	// set shader resc view and sampler
-	deviceAndContext.context->PSSetShaderResources(0, 1, pShaderResView.GetAddressOf());
-	deviceAndContext.context->PSSetSamplers(0, 1, pSamplerState.GetAddressOf());
+	deviceAndContext.context->PSSetShaderResources(0, 1, texture.pShaderResView.GetAddressOf());
+	deviceAndContext.context->PSSetSamplers(0, 1, texture.pSamplerState.GetAddressOf());
 
 	// Create input layout
 	ComPtr<ID3D11InputLayout> vertLayout;
