50cd253d772fca261b3c9341f063bdb0524ac84d
Timothy Young <timothyoung97@gmail.com>
2023-09-03 19:58:50 +0800

    Update hlsl to take in direction light source

--- RenderingEngine/shaders/triangle.hlsl
+++ RenderingEngine/shaders/triangle.hlsl
@@ -1,12 +1,21 @@
+struct Light {
+    float3 dir;
+    float4 ambient;
+    float4 diffuse;
+};
+
+// Global 
 cbuffer constBuffer : register(b0) {
     matrix viewProjection;
-}
+    Light light;
+};
 
+// Per Object
 cbuffer constBuffer2 : register(b1) {
     matrix transformation;
     float4 color;
     uint isWithTexture;
-}
+};
 
 Texture2D ObjTexture;
 SamplerState ObjSamplerState;
@@ -14,28 +23,50 @@ SamplerState ObjSamplerState;
 // Vertex Shader
 void vs_main (
     in float3 inPosition : POSITION,
+    in float3 inNormal : NORMAL,
     in float2 inTexCoord : TEXCOORD,
     out float4 outPosition : SV_POSITION,
+    out float4 outNormal : NORMAL,
     out float2 outTexCoord : TEXCOORD
 ) 
 {   
+    // Position
     float4 tempInPos = float4(inPosition, 1);
     float4 localPos = mul(transformation, tempInPos);
     outPosition = mul(viewProjection, localPos);
 
+    // Normal
+    float4 tempInNormal = float4(inNormal, 1);
+    float4 localNormal = mul(transformation, tempInNormal);
+    outNormal = mul(viewProjection, localNormal);
+
+    // Texture
     outTexCoord = inTexCoord;
-}
+};
 
 // Pixel Shader
 void ps_main (
-    in float4 outPosition : SV_POSITION,
+    in float4 vOutPosition : SV_POSITION,
+    in float4 vOutNormal : NORMAL,
     in float2 vOutTexCoord : TEXCOORD,
     out float4 outTarget: SV_TARGET
 ) 
 {   
+    // normal
+    vOutNormal = normalize(vOutNormal);
+
+    // uv texture
+    float4 sampleTexture;
     if (isWithTexture) {
-        outTarget = ObjTexture.Sample(ObjSamplerState, vOutTexCoord);
+        sampleTexture = ObjTexture.Sample(ObjSamplerState, vOutTexCoord);
     } else {
-        outTarget = color;
+        sampleTexture = color;
     }
-}
+
+    // Final Color
+    float3 fColor;
+    fColor = sampleTexture.xyz * light.ambient.xyz;
+    fColor += saturate(dot(light.dir, vOutNormal.xyz) * light.diffuse.xyz * sampleTexture.xyz);
+
+    outTarget = float4(fColor, sampleTexture.a);
+};
