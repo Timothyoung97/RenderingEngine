e17a501fa36043817e88a7f30bd7fb25c2e91c0a
Timothy Young <timothyoung97@gmail.com>
2023-09-04 13:45:36 +0800

    Add point light properties to light

--- RenderingEngine/light.h
+++ RenderingEngine/light.h
@@ -6,7 +6,11 @@ using namespace DirectX;
 
 struct Light {
 	XMFLOAT3 direction;
-	float pad;
+	float pad1;
+	XMFLOAT3 pos;
+	float range;
+	XMFLOAT3 att;
+	float pad2;
 	XMFLOAT4 ambient;
 	XMFLOAT4 diffuse;
 };
