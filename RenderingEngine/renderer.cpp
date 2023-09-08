a3006b76a9443e52d1339e8c6d4990f1b51e4200
Timothy Young <timothyoung97@gmail.com>
2023-09-08 14:52:27 +0800

    Set debug render call

--- RenderingEngine/renderer.cpp
+++ RenderingEngine/renderer.cpp
@@ -63,4 +63,64 @@ void Renderer::draw(ID3D11Device* device, ID3D11DeviceContext* context, ID3D11Ra
 		context->DrawIndexed(currObj.pObjMesh->indexSize, 0, 0);
 	}
 }
+
+void Renderer::debugDraw(ID3D11Device* device, ID3D11DeviceContext* context, ID3D11RasterizerState* rasterizerState, tre::ConstantBuffer& cb, const std::vector<Object>& objQ, float ritterBsScale) {
+
+	context->RSSetState(rasterizerState);
+
+	for (int i = 0; i < objQ.size(); i++) {
+
+		const tre::Object& currObj = objQ[i];
+
+		//Set vertex buffer
+		UINT vertexStride = sizeof(Vertex);
+		UINT offset = 0;
+		context->IASetVertexBuffers(0, 1, currObj.boundingSphere->pVertexBuffer.GetAddressOf(), &vertexStride, &offset);
+
+		//Set index buffer
+		context->IASetIndexBuffer(currObj.boundingSphere->pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0);
+
+		//set shader resc view and sampler
+		context->PSSetShaderResources(0, 1, currObj.pObjTexture->pShaderResView.GetAddressOf());
+
+		//Config const buffer
+		cb.constBufferRescModel.transformationLocal = XMMatrixMultiply(
+			XMMatrixScaling(currObj.objScale.x * ritterBsScale, currObj.objScale.y * ritterBsScale, currObj.objScale.z * ritterBsScale),
+			XMMatrixMultiply(
+				XMMatrixRotationRollPitchYaw(XMConvertToRadians(currObj.objRotation.x), XMConvertToRadians(currObj.objRotation.x), XMConvertToRadians(currObj.objRotation.x)),
+				XMMatrixTranslation(currObj.objPos.x, currObj.objPos.y, currObj.objPos.z)
+			)
+		);
+
+		XMMATRIX normalMatrix = XMMatrixTranspose(XMMatrixInverse(nullptr, cb.constBufferRescModel.transformationLocal));
+
+		XMFLOAT3X3 normalFloat3x3;
+		XMStoreFloat3x3(&normalFloat3x3, normalMatrix);
+
+		cb.constBufferRescModel.normalMatrix = XMLoadFloat3x3(&normalFloat3x3);
+
+		cb.constBufferRescModel.isWithTexture = currObj.isObjWithTexture;
+		cb.constBufferRescModel.hasNormalMap = currObj.isObjWithNormalMap;
+		cb.constBufferRescModel.color = currObj.objColor;
+
+		// set normal map
+		if (cb.constBufferRescModel.hasNormalMap) {
+			context->PSSetShaderResources(1, 1, currObj.pObjNormalMap->pShaderResView.GetAddressOf());
+		}
+
+		//map to data to subresouce
+		cb.csd.pSysMem = &cb.constBufferRescModel;
+
+		CHECK_DX_ERROR(device->CreateBuffer(
+			&cb.constantBufferDescModel, &cb.csd, cb.pConstBuffer.GetAddressOf()
+		));
+
+		//Set const buffer for pixel and vertex shader
+		context->VSSetConstantBuffers(1u, 1u, cb.pConstBuffer.GetAddressOf());
+		context->PSSetConstantBuffers(1u, 1u, cb.pConstBuffer.GetAddressOf());
+
+		context->DrawIndexed(currObj.boundingSphere->indexSize, 0, 0);
+	}
+}
+
 }
