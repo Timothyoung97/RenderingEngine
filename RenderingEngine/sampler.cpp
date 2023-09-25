337dc9c2dd45df66033240dba72bc300ba15054b
Timothy Young <timothyoung97@gmail.com>
2023-09-25 14:26:23 +0800

    Change sampler to wrap UV instead of clamp

--- RenderingEngine/sampler.cpp
+++ RenderingEngine/sampler.cpp
@@ -8,9 +8,9 @@ Sampler::Sampler(ID3D11Device* device) {
 	// Create sampler state
 	D3D11_SAMPLER_DESC samplerDesc;
 	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
-	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
-	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
-	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
+	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.MipLODBias = 0;
 	samplerDesc.MaxAnisotropy = 1;
 	samplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
