dcd7d67c1ef0d8488dd748b6727e3e003b771178
Timothy Young <timothyoung97@gmail.com>
2023-10-09 11:50:36 +0800

    Move UV and vertices coord into the func

--- RenderingEngine/shaders/fullscreenRendering.hlsl
+++ RenderingEngine/shaders/fullscreenRendering.hlsl
@@ -1,26 +1,27 @@
-static const float4 vertices[6] = {
+void vs_fullscreen(
+    in uint vertexID: SV_VertexID,
+    out float4 outPosition: SV_POSITION,
+    out float2 outTexCoord: TEXCOORD0
+) {
+
+    float4 vertices[6] = {
     float4(-1.f,  1.f, 0.f, 1.f), 
     float4(-1.f, -1.f, 0.f, 1.f), 
     float4( 1.f,  1.f, 0.f, 1.f), 
     float4(-1.f, -1.f, 0.f, 1.f), 
     float4( 1.f, -1.f, 0.f, 1.f),
     float4( 1.f,  1.f, 0.f, 1.f) 
-};
+    };
 
-static const float2 uv[6] = {
-    float2(0, 0),
-    float2(0, 1),
-    float2(1, 0),
-    float2(0, 1),
-    float2(1, 1),
-    float2(1, 0)
-};
+    float2 uv[6] = {
+        float2(0, 0),
+        float2(0, 1),
+        float2(1, 0),
+        float2(0, 1),
+        float2(1, 1),
+        float2(1, 0)
+    };
 
-void vs_fullscreen(
-    in uint vertexID: SV_VertexID,
-    out float4 outPosition: SV_POSITION,
-    out float2 outTexCoord: TEXCOORD0
-) {
     outPosition = vertices[vertexID];
     outTexCoord = uv[vertexID];
 }
