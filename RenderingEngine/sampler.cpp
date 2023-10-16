5eae3bc0bd56616717f375cb990d8e41d8a4a369
Timothy Young <timothyoung97@gmail.com>
2023-10-16 11:46:18 +0800

    Update sampler's naming

--- RenderingEngine/sampler.cpp
+++ RenderingEngine/sampler.cpp
@@ -40,22 +40,22 @@ void Sampler::create(ID3D11Device* device) {
 	CHECK_DX_ERROR(device->CreateSamplerState(&shadowSamplerDesc, pSamplerStateMipPtWhiteBorder.GetAddressOf()));
 
 	// Create sampler state for ssao
-	D3D11_SAMPLER_DESC shadowSamplerDesc;
-	ZeroMemory(&shadowSamplerDesc, sizeof(D3D11_SAMPLER_DESC));
-	shadowSamplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
-	shadowSamplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
-	shadowSamplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
-	shadowSamplerDesc.BorderColor[0] = 1.0f;
-	shadowSamplerDesc.BorderColor[1] = 1.0f;
-	shadowSamplerDesc.BorderColor[2] = 1.0f;
-	shadowSamplerDesc.BorderColor[3] = 1.0f;
-	shadowSamplerDesc.MinLOD = 0.f;
-	shadowSamplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
-	shadowSamplerDesc.MipLODBias = 0.f;
-	shadowSamplerDesc.MaxAnisotropy = 0;
-	shadowSamplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
-	shadowSamplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
+	D3D11_SAMPLER_DESC ssaoSamplerDesc;
+	ZeroMemory(&ssaoSamplerDesc, sizeof(D3D11_SAMPLER_DESC));
+	ssaoSamplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	ssaoSamplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	ssaoSamplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	ssaoSamplerDesc.BorderColor[0] = 1.0f;
+	ssaoSamplerDesc.BorderColor[1] = 1.0f;
+	ssaoSamplerDesc.BorderColor[2] = 1.0f;
+	ssaoSamplerDesc.BorderColor[3] = 1.0f;
+	ssaoSamplerDesc.MinLOD = 0.f;
+	ssaoSamplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+	ssaoSamplerDesc.MipLODBias = 0.f;
+	ssaoSamplerDesc.MaxAnisotropy = 0;
+	ssaoSamplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+	ssaoSamplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
 
-	CHECK_DX_ERROR(device->CreateSamplerState(&shadowSamplerDesc, pSamplerStateMipPtWrap.GetAddressOf()));
+	CHECK_DX_ERROR(device->CreateSamplerState(&ssaoSamplerDesc, pSamplerStateMipPtWrap.GetAddressOf()));
 }
 }
