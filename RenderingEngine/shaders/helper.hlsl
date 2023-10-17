2ab76d4414bf876430046b4c043c9f03b841e0c4
Timothy Young <timothyoung97@gmail.com>
2023-10-17 15:16:12 +0800

    add helper func

--- RenderingEngine/shaders/helper.hlsl
+++ RenderingEngine/shaders/helper.hlsl
@@ -5,3 +5,34 @@ static float2 clipToScreenSpace(float2 xy) {
 static float2 screenToClipSpace(float2 xy, float2 viewportDimension) {
     return (xy / viewportDimension) * float2(2.0f, -2.0) + float2(-1, 1);
 }
+
+static float3x3 CalculateTBN(float3 p, float3 n, float2 tex) {
+    float3 dp1 = ddx(p);
+    float3 dp2 = ddy(p);
+    float2 duv1 = ddx(tex);
+    float2 duv2 = ddy(tex);
+
+    float3x3 M = float3x3(dp1, dp2, cross(dp1, dp2));
+    float2x3 inverseM = float2x3(cross(M[1], M[2]), cross(M[2], M[0]));
+    float3 t = normalize(mul(float2(duv1.x, duv2.x), inverseM));
+    float3 b = normalize(mul(float2(duv1.y, duv2.y), inverseM));
+    return float3x3(t, b, n);
+}
+
+// Rotation with angle (in radians) and axis
+float3x3 AngleAxis3x3(float angle, float3 axis)
+{
+    float c, s;
+    sincos(angle, s, c);
+
+    float t = 1 - c;
+    float x = axis.x;
+    float y = axis.y;
+    float z = axis.z;
+
+    return float3x3(
+        t * x * x + c,      t * x * y - s * z,  t * x * z + s * y,
+        t * x * y + s * z,  t * y * y + c,      t * y * z - s * x,
+        t * x * z - s * y,  t * y * z + s * x,  t * z * z + c
+    );
+}
