2516e575c371262afbd9c6fd94cb302c879b692f
Timothy Young <timothyoung97@gmail.com>
2023-10-24 17:56:08 +0800

    Add hdr pixel shader

--- RenderingEngine/shaders/compile.bat
+++ RenderingEngine/shaders/compile.bat
@@ -11,5 +11,6 @@ del *.bin
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\fxc.exe" /nologo /E ps_lightingLocalPass /T ps_5_0 /Fo pixel_shader_deferred_lighting_local.bin deferredRenderingLightingLocal.hlsl
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\fxc.exe" /nologo /E ps_ssao /T ps_5_0 /Fo pixel_shader_ssao_rendering.bin ssaoRendering.hlsl
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\fxc.exe" /nologo /E ps_texture_blur /T ps_5_0 /Fo pixel_shader_texture_blur.bin textureBlur.hlsl
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\fxc.exe" /nologo /E ps_hdr_tonedown /T ps_5_0 /Fo pixel_shader_hdr_rendering.bin hdrRendering.hlsl
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\fxc.exe" /nologo /E cs_updateLightPosition /T cs_5_0 /Fo compute_shader_ptLight_movement.bin ptLightMovement.hlsl
  
