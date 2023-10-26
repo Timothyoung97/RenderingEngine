e255efd9131071e929722fe4808b29d25e04a3c8
Timothy Young <timothyoung97@gmail.com>
2023-10-26 15:29:18 +0800

    Move commonly used struct into a separate file

--- RenderingEngine/shaders/forwardRendering.hlsl
+++ RenderingEngine/shaders/forwardRendering.hlsl
@@ -1,21 +1,5 @@
 #include "helper.hlsl"
-
-struct Light {
-    float3 dir;
-    float pad;
-    float4 ambient;
-    float4 diffuse;
-};
-
-struct PointLight {
-    float3 pos;
-    float range;
-    float3 att;
-    float pad;
-    float4 diffuse;
-    float2 yawPitch;
-    float2 pad2;
-};
+#include "globalResources.hlsl"
 
 // Global 
 cbuffer constBuffer : register(b0) {
