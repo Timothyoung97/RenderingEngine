e13b23fde6a15f2db7d00647f0123904fb767097
Timothy Young <timothyoung97@gmail.com>
2023-10-30 11:19:19 +0800

    Create vertex shader for instanced rendering

---
+++ RenderingEngine/shaders/vs_instancedRendering.hlsl
@@ -0,0 +1,77 @@
+#include "globalResources.hlsl"
+
+// Global 
+cbuffer constBuffer : register(b0) {
+    float2 viewportDimension; // (width, height)
+    float2 pad;
+    float4 camPos;
+    matrix viewProjection;
+    matrix invViewProjection;
+    matrix lightviewProjection[4];
+    float4 planeIntervals;
+    Light dirLight;
+    uint numPtLights;
+    float2 shadowMapDimension;
+    int csmDebugSwitch;
+    int ssaoSwitch;
+};
+
+// Batch offset
+cbuffer constBuffer2 : register(b1) {
+    uint offset;
+};
+
+struct InstanceInfo {
+    matrix transformation;
+    matrix normalMatrix;
+    float4 color;
+    uint isWithTexture;
+    uint hasNormMap;
+};
+
+StructuredBuffer<InstanceInfo> instanceInfoQueue : register(t0);
+
+// Vertex Shader
+void vs_instancedRendering (
+    uint instanceID : SV_InstanceID,
+    in float3 inPosition : POSITION,
+    in float3 inNormal : NORMAL,
+    in float3 inTangent : TANGENT,
+    in float2 inTexCoord : TEXCOORD,
+    out float4 outPosition : SV_POSITION,
+    out float4 outWorldPosition : TEXCOORD0,
+    out float4 outNormal : TEXCOORD1,
+    out float4 outTangent : TEXCOORD2,
+    out float2 outTexCoord : TEXCOORD3,
+    out float4 outColor : TEXCOORD4,
+    out float4 outTextuerInfo : TEXCOORD5
+) 
+{   
+
+    InstanceInfo iInfo = instanceInfoQueue[offset + instanceID];
+
+    // Position
+    float4 localPos = float4(inPosition, 1);
+    float4 worldPos = mul(iInfo.transformation, localPos);
+    outPosition = mul(viewProjection, worldPos);
+
+    // world position
+    outWorldPosition = worldPos;
+
+    // Normal
+    float4 tempInNormal = float4(inNormal, 0);
+    outNormal = normalize(mul(iInfo.normalMatrix, tempInNormal));
+
+    // Tangent
+    float4 tempInTangent = float4(inTangent, 0);
+    outTangent = normalize(mul(iInfo.normalMatrix, tempInTangent)); // using normal matrix to move tangent
+
+    // Texture
+    outTexCoord = inTexCoord;
+
+    // Color 
+    outColor = iInfo.color;
+
+    // Texture Info
+    outTextuerInfo = float4(iInfo.isWithTexture, iInfo.hasNormMap, .0f, .0f);
+};
