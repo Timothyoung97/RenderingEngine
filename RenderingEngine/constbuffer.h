e6d9da801641092a02a13c012da55969c981e48c
Timothy Young <timothyoung97@gmail.com>
2023-11-21 14:54:39 +0800

    Comment out deprecated code

--- RenderingEngine/constbuffer.h
+++ RenderingEngine/constbuffer.h
@@ -102,25 +102,26 @@ public:
 	static BatchInfoStruct createBatchInfoStruct(int batchOffset);
 
 	///////////////// --DEPRECATED-- /////////////////
-	static ID3D11Buffer* setCamConstBuffer(
-		ID3D11Device* device, ID3D11DeviceContext* context, 
-		XMVECTOR camPos, 
-		XMMATRIX viewProjection, 
-		const std::vector<XMMATRIX>& lightViewProjection, 
-		XMFLOAT4 planeIntervals, 
-		const tre::Light& dirLight, 
-		int numOfPointLight,
-		XMFLOAT2 shadowMapDimension,
-		int csmDebugSwitch,
-		int ssaoSwtich
-	);
-	static ID3D11Buffer* setObjConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, XMMATRIX transformationLocal, XMFLOAT4 color, UINT isWithTexture, UINT hasNormalMap);
-	static ID3D11Buffer* setLightingVolumeConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, int currPtLightIdx);
-	static ID3D11Buffer* setSSAOKernalConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, const std::vector<XMFLOAT4>& kernalSamples, float sampleRadius);
-	static ID3D11Buffer* setHDRConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, float middleGrey);
-	static ID3D11Buffer* setLuminaceConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, XMFLOAT2 luminance, float timeCoeff);
-	static ID3D11Buffer* setLightViewProjectionConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, XMMATRIX viewProjection);
-	static ID3D11Buffer* setBatchInfoConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, int batchOffset);
+	
+	//static ID3D11Buffer* setCamConstBuffer(
+	//	ID3D11Device* device, ID3D11DeviceContext* context, 
+	//	XMVECTOR camPos, 
+	//	XMMATRIX viewProjection, 
+	//	const std::vector<XMMATRIX>& lightViewProjection, 
+	//	XMFLOAT4 planeIntervals, 
+	//	const tre::Light& dirLight, 
+	//	int numOfPointLight,
+	//	XMFLOAT2 shadowMapDimension,
+	//	int csmDebugSwitch,
+	//	int ssaoSwtich
+	//);
+	//static ID3D11Buffer* setObjConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, XMMATRIX transformationLocal, XMFLOAT4 color, UINT isWithTexture, UINT hasNormalMap);
+	//static ID3D11Buffer* setLightingVolumeConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, int currPtLightIdx);
+	//static ID3D11Buffer* setSSAOKernalConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, const std::vector<XMFLOAT4>& kernalSamples, float sampleRadius);
+	//static ID3D11Buffer* setHDRConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, float middleGrey);
+	//static ID3D11Buffer* setLuminaceConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, XMFLOAT2 luminance, float timeCoeff);
+	//static ID3D11Buffer* setLightViewProjectionConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, XMMATRIX viewProjection);
+	//static ID3D11Buffer* setBatchInfoConstBuffer(ID3D11Device* device, ID3D11DeviceContext* context, int batchOffset);
 };
 
 }
