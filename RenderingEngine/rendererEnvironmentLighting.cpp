282cb612044b5c12fafd5d07e776d4eed10a25a7
Timothy Young <timothyoung97@gmail.com>
2024-03-27 20:48:33 +0800

    Organize headers

--- RenderingEngine/rendererEnvironmentLighting.cpp
+++ RenderingEngine/rendererEnvironmentLighting.cpp
@@ -1,10 +1,5 @@
 #include "rendererEnvironmentLighting.h"
 
-#include "utility.h"
-#include "engine.h"
-
-extern std::shared_ptr<tre::Engine> pEngine;
-
 namespace tre {
 
 RendererEnvironmentLighting::RendererEnvironmentLighting() {
